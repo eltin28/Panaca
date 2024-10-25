8cfac9982c12df58ae99329bbeb2782f5d40fe6a
eltin28 <juanj.herrerao@uqvirtual.edu.co>
2024-10-25 15:52:08 -0500

    Avance en la vista de eventos y del evento

---
+++ src/app/componentes/evento-unidad/evento-unidad.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evento-unidad',
+  standalone: true,
+  imports: [],
+  templateUrl: './evento-unidad.component.html',
+  styleUrl: './evento-unidad.component.css'
+})
+export class EventoUnidadComponent {
+
+}

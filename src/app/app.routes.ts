6d9d04f32c23852aa53c3c04ab5c52c0fcdfeaf6
eltin28 <juanj.herrerao@uqvirtual.edu.co>
2024-10-28 22:49:26 -0500

    Vista carrito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { Component } from '@angular/core';
 import { InicioComponent } from './componentes/inicio/inicio.component';
-// import { LoginComponent } from './componentes/login/login.component';
+import { LoginComponent } from './componentes/login/login.component';
 import { RegistroLoginComponent } from './componentes/registro-login/registro.component';
 import { CrearEventoComponent } from './componentes/crear-evento/crear-evento.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { EventosComponent } from './componentes/eventos/eventos.component';
+import { EventoUnidadComponent } from './componentes/evento-unidad/evento-unidad.component';
+import { CarritoComponent } from './componentes/carrito/carrito.component';
 
 
 export const routes: Routes = [
@@ -14,7 +16,9 @@ export const routes: Routes = [
    // { path: 'login', component: LoginComponent },
    { path: 'login', component: RegistroLoginComponent },
    { path: 'crear-evento', component: CrearEventoComponent },
-   { path: 'eventos', component: EventosComponent},
+   { path: 'eventos', component: EventosComponent },
+   { path: 'evento', component: EventoUnidadComponent },
+   { path: 'carrito', component: CarritoComponent },
    { path: "**", pathMatch: "full", redirectTo: "" }
 ];
 
--- src/app/componentes/evento-unidad/evento-unidad.component.ts
+++ src/app/componentes/evento-unidad/evento-unidad.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-evento-unidad',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, FormsModule ],
   templateUrl: './evento-unidad.component.html',
   styleUrl: './evento-unidad.component.css'
 })
 export class EventoUnidadComponent {
 
+
+  localidades = [
+    { nombre: 'VIP', precio: 100 },
+    { nombre: 'General', precio: 50 },
+    { nombre: 'Balcón', precio: 30 }
+  ];
+
+  cantidades = [1, 2, 3, 4, 5];
+  locations = [
+    { selectedLocalidad: '', cantidad: null }
+  ];
+
+  agregarUbicacion() {
+    if (this.locations.length < 3) {
+      this.locations.push({ selectedLocalidad: '', cantidad: null });
+    }
+  }
+
+  agregarAlCarrito() {
+    console.log('Entradas seleccionadas:', this.locations);
+    // Aquí podrías llamar un servicio que agregue las entradas al carrito o procesarlas
+  }
 }

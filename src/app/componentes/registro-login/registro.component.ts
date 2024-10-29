e2260c20238b52c49e64c8b63a9cdc227696f0c6
eltin28 <juanj.herrerao@uqvirtual.edu.co>
2024-10-28 23:58:44 -0500

    Formulario de registro y login con registro reactivo correctamente

--- src/app/componentes/registro-login/registro.component.ts
+++ src/app/componentes/registro-login/registro.component.ts
@@ -1,19 +1,17 @@
 import { Component, OnInit, Inject } from '@angular/core';
-// import { RegistroClienteDTO } from '../../dto/registro-cliente-dto';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FormsModule } from '@angular/forms';
+import { AbstractControlOptions, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { DOCUMENT } from '@angular/common';
 // import { PublicoService } from '../../servicios/publico.service';
 // import { AuthService } from '../../servicios/auth.service';
 // import { AlertaComponent } from '../alerta/alerta.component';
 // import { Alerta } from '../../dto/alerta';
-// import { LoginDTO } from '../../dto/login-dto';
 // import { TokenService } from '../../servicios/token.service';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [FormsModule, FontAwesomeModule, 
+  imports: [ReactiveFormsModule , FontAwesomeModule, 
     // AlertaComponent
   ],
   templateUrl: './registro.component.html',
@@ -22,44 +20,65 @@ import { DOCUMENT } from '@angular/common';
 export class RegistroLoginComponent implements OnInit {
 
   container: HTMLElement | null = null;
-  // registroClienteDTO: RegistroClienteDTO;
-  // loginDTO: LoginDTO;
-  archivos!:FileList;
   showPassword = false;
   activeIcon = 'fa-eye'; //el icono de ojo abierto está activo
   // alerta!:Alerta;
+  registroForm!: FormGroup;
+  loginForm!: FormGroup;
 
 
-  constructor(@Inject(DOCUMENT) private document: Document, 
+  constructor(@Inject(DOCUMENT, ) private document: Document, private formBuilder: FormBuilder,
   // private publicoService: PublicoService, 
     // private authService: AuthService, 
     // private tokenService: TokenService
   ) {
-    // this.registroClienteDTO = new RegistroClienteDTO();
-    // this.loginDTO = new LoginDTO();
+
+    this.crearFormulario();
   }
 
-  // public sonIguales(): boolean {
-  //   return this.registroClienteDTO.password == this.registroClienteDTO.confirmaPassword;
-  // }
+  private crearFormulario() {
 
-  public onFileChange(event: any) {
-    if (event.target.files.length > 0) {
-      this.archivos = event.target.files;
-      // this.registroClienteDTO.fotoPerfil = this.archivos[0].name;
-    }
-  }
+    this.registroForm = this.formBuilder.group(
+      {
+      cedula: ['', [Validators.required]],
+      nombre: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      direccion: ['', [Validators.required]],
+      telefono: ['', [Validators.required, Validators.maxLength(10)]],
+      password: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(7)]],
+      confirmaPassword: ['', [Validators.required]]
+      },
+      { 
+        validators: [this.passwordsMatchValidator]
+      } as AbstractControlOptions
+    );
+
+    this.loginForm = this.formBuilder.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required]]
+      }
+    );
+
+   }
+   
 
   public registrar() {
-    // this.authService.registrarCliente(this.registroClienteDTO).subscribe({
-    //   next: (data) => {
-    //     this.alerta = new Alerta(data.respuesta, "success");
-    //   },
-    //   error: (error) => {
-    //     this.alerta = new Alerta(error.error.respuesta, "danger");
-    //   }
-    // });
+    console.log(this.registroForm.value);
   }
+
+  public login() {
+    console.log(this.loginForm.value);
+  }
+
+  passwordsMatchValidator(formGroup: FormGroup) {
+    const password = formGroup.get('password')?.value;
+    const confirmaPassword = formGroup.get('confirmaPassword')?.value;
+   
+   
+    // Si las contraseñas no coinciden, devuelve un error, de lo contrario, null
+    return password == confirmaPassword ? null : { passwordsMismatch: true };
+  }   
 
   ngOnInit(): void {
     this.container = this.document.getElementById('container'); // Get container reference
@@ -85,19 +104,6 @@ export class RegistroLoginComponent implements OnInit {
   togglePasswordVisibility() {
     this.showPassword =!this.showPassword;
     this.activeIcon = this.activeIcon === 'fa-eye'? 'fa-eye-slash' : 'fa-eye'; // Cambia el icono activo
-  }
-
-  //__________________________________________________________________________________________________________________
-  
-  public login() {
-      // this.authService.loginCliente(this.loginDTO).subscribe({
-      //   next: data => {
-      //     this.tokenService.login(data.respuesta.token);
-      //   },
-      //   error: error => {
-      //     this.alerta = new Alerta(error.error.respuesta, "danger" );
-      //   }
-      //   });
   }
   
 }

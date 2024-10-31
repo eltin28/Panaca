e5fb0e47a6056f9d33f0b965e2fa47006d1ccf41
eltin28 <juanj.herrerao@uqvirtual.edu.co>
2024-10-31 11:07:04 -0500

    Cambio contrasena

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,23 @@
 import { Routes } from '@angular/router';
 import { Component } from '@angular/core';
 import { InicioComponent } from './componentes/inicio/inicio.component';
-import { LoginComponent } from './componentes/login/login.component';
 import { RegistroLoginComponent } from './componentes/registro-login/registro.component';
 import { CrearEventoComponent } from './componentes/crear-evento/crear-evento.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { EventosComponent } from './componentes/eventos/eventos.component';
 import { EventoUnidadComponent } from './componentes/evento-unidad/evento-unidad.component';
 import { CarritoComponent } from './componentes/carrito/carrito.component';
+import { CambioContrasenaComponent } from './componentes/cambio-contrasena/cambio-contrasena.component'
 
 
 export const routes: Routes = [
    { path: '', component: InicioComponent },
-   // { path: 'header', component: HeaderComponent},
-   // { path: 'login', component: LoginComponent },
    { path: 'login', component: RegistroLoginComponent },
    { path: 'crear-evento', component: CrearEventoComponent },
    { path: 'eventos', component: EventosComponent },
    { path: 'evento', component: EventoUnidadComponent },
    { path: 'carrito', component: CarritoComponent },
+   { path: 'cambio-contrasena', component: CambioContrasenaComponent },
    { path: "**", pathMatch: "full", redirectTo: "" }
 ];
 

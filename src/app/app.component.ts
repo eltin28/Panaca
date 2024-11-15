e9e6d83478c26206d181533a27c236308ddf9fa2
Juan Camilo <jalbaranmontoya@gmail.com>
2024-11-15 01:40:32 -0500

    Pequeña correcion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SearchBarComponent } from './componentes/search-bar/search-bar.component';
-import { CarouselComponent } from './componentes/carousel/carousel.component';
 import { FooterComponent } from "./componentes/footer/footer.component";
-import { InicioComponent } from "./componentes/inicio/inicio.component";
 import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
 import { NavbarAdminComponent } from './componentes/navbar-admin/navbar-admin.component';
@@ -12,7 +10,7 @@ import { TokenService } from './servicios/token.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, CommonModule, SearchBarComponent, CarouselComponent, InicioComponent, FooterComponent, NavbarComponent, NavbarAdminComponent],
+  imports: [RouterModule, CommonModule, SearchBarComponent, FooterComponent, NavbarComponent, NavbarAdminComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
